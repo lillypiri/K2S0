0e121ffb75804afa9275d1ab26590edb1e919ed1
Caitlin <c_palmerbright@hotmail.com>
2017-04-11 13:13:26 +1000

    added value of previous guess to hint message

--- app.rb
+++ app.rb
@@ -12,26 +12,23 @@ end
 
 
 post '/guess' do
-  # { guess: params['guess']}.to_json
   guesses = params['guess_count'].to_i
+  correct = params['correct_number'].to_i
+  guess = params['guess']
   case
-  when params['guess'].to_i == params['correct_number'].to_i
-      #change message to congratulations message
-      # change button to 'new game' button
-      # hide the input
-      # change heading?
+  when guess.to_i == correct
     { message: "Yay! You guessed the number!", done: "Finished"}.to_json
   when guesses >= 9
     { message: "oh noooooooos. You ran out of guesses!", done: "Finished"}.to_json
-    when !is_a_number?(params['guess'])
+    when !is_a_number?(guess)
       guesses += 1
-      { message: "That's not an integer, dummy!", guess_count: guesses}.to_json
-    when params['guess'].to_i > params['correct_number'].to_i
+      { message: "#{guess} isn't an integer, dummy!", guess_count: guesses}.to_json
+    when guess.to_i > correct
       guesses += 1
-      { message: "Too high! Guess again!", guess_count: guesses}.to_json
-    when params['guess'].to_i < params['correct_number'].to_i
+      { message: "#{guess} is too high! Guess again!", guess_count: guesses}.to_json
+    when guess.to_i < correct
       guesses += 1
-      { message: "Too low! Guess again!", guess_count: guesses}.to_json
+      { message: "#{guess} is too low! Guess again!", guess_count: guesses}.to_json
   end
 end
 
